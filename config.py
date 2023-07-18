e83e07e4a3e80ab8a327de43e9806533a795ad31
Max Base <MaxBaseCode@Gmail.Com>
2023-07-18 13:15:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
-# Set user information with their respective Wakatime API keys
+# User information with their respective Wakatime API keys
 USERS = {
 	'Max': '.............',
 }
 
+# Time
 TIME_ZONE = 'Asia/Tehran'
+SEND_AT_HOUR = 0
+SEND_AT_MINUTE = 0
+SEND_AT_SECOND = 0
+SEND_AT_MICROSECOND = 0
 
 # Telegram Bot Token and Chat ID
-bot_token = '473681081:xxxxxxxxxxx'
-chat_id = '-xxxxxxxxxxx'
+BOT_TOKEN = '473681081:xxxxxxxxxxx'
+CHAT_ID = '-xxxxxxxxxxx'
 
 # Set start and end date for coding activity report
-today = datetime.now().strftime('%Y-%m-%d')
-# today = '2023/07/16'
-start_date = today
-end_date = today
+TODAY = datetime.now().strftime('%Y-%m-%d')
+START_DATE = today
+END_dATE = today
 
-# Set email parameters
-email_subject = f"Coding Activity Report - {today}"
-email_sender = 'max@asrez.com'
-email_receivers = [
+# Email
+EMAIL_SUBJECT = f"Coding Activity Report - {today}"
+EMAIL_SENDER = 'max@asrez.com'
+EMAIL_RECEIVERS = [
   'maxbasecode@gmail.com',
   '98@hi2.in'
 ]
-email_password = 'XXXX'
+EMAIL_PASSWORD = 'XXXX'
